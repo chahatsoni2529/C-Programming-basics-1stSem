4db4c16f08b915e59505c1554e864caf25b0d2c7
Chahat Soni <chahatsoni2529@gmail.com>
2026-01-05 23:12:46 +0530

    Implement basic arithmetic operations in C
    Added a C program for basic arithmetic operations including addition, subtraction, and multiplication.

---
+++ 01-Basics/arithematic_operations.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+    // int a, b, sum;
+    // printf("Enter the value of integers\n");
+    // scanf("%d %d", &a, &b);
+    // sum = a + b;
+    // printf("Sum = %d", sum);  
+    
+    //  int a, b, sub;
+    // printf("Enter the value of integers\n");
+    // scanf("%d %d", &a, &b);
+    // sub = a - b;
+    // printf("Sub = %d", sub);  
+
+    //  int a, b, mul;
+    // printf("Enter the value of integers\n");
+    // scanf("%d %d", &a, &b);
+    // mul = a * b;
+    // printf("Mul = %d", mul);  
+    return 0;
+}
