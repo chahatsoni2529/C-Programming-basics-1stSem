b6df769dedaabaf75cd3f4aa554556a743018570
Chahat Soni <chahatsoni2529@gmail.com>
2026-01-06 13:40:30 +0530

    Add pass/fail check based on marks input

---
+++ 02_if_else/check_pass_fail.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+  int marks;
+    printf("Enter your marks\n");
+    scanf("%d", &marks);
+
+      if(marks>=33){
+      printf("Pass");
+      }
+
+  else{
+    printf("Fail");
+  }
+
+    return 0;
+}
