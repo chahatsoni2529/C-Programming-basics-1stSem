161a5aa2020b25bf23175e9e7b7eb1da1d232401
Chahat Soni <chahatsoni2529@gmail.com>
2026-01-06 13:30:44 +0530

    Implement zero and non-zero number check
    Fix the condition in the if statement to check for zero.

---
+++ 02_if_else/zero_non-zero.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+  int n;
+  printf("Enter a number");
+  scanf("%d", &n);
+
+  if(n=0){
+    printf("Number is zero");
+  }
+  else{
+    printf("Number is non-zero");
+  }
+  return 0;
+}
